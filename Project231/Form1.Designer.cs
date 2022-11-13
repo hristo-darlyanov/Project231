c9c6e1fa83b1e30873deb08648952faeddf5620e
hristo-darlyanov <memeda963@gmail.com>
2022-11-13 17:28:56 +0200

    Add project files.

---
+++ Project231/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Project231
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.helpProvider1 = new System.Windows.Forms.HelpProvider();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.calcBtn = new System.Windows.Forms.Button();
+            this.medianLabel = new System.Windows.Forms.Label();
+            this.averageLabel = new System.Windows.Forms.Label();
+            this.modeLabel = new System.Windows.Forms.Label();
+            this.histogram = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.histogram)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(16, 28);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(850, 45);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Enter Nums";
+            // 
+            // calcBtn
+            // 
+            this.calcBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.calcBtn.Location = new System.Drawing.Point(876, 28);
+            this.calcBtn.Name = "calcBtn";
+            this.calcBtn.Size = new System.Drawing.Size(164, 45);
+            this.calcBtn.TabIndex = 1;
+            this.calcBtn.Text = "Calculate";
+            this.calcBtn.UseVisualStyleBackColor = true;
+            this.calcBtn.Click += new System.EventHandler(this.calcBtn_Click);
+            // 
+            // medianLabel
+            // 
+            this.medianLabel.AutoSize = true;
+            this.medianLabel.Location = new System.Drawing.Point(12, 155);
+            this.medianLabel.Name = "medianLabel";
+            this.medianLabel.Size = new System.Drawing.Size(147, 39);
+            this.medianLabel.TabIndex = 2;
+            this.medianLabel.Text = "Median: ";
+            this.medianLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // averageLabel
+            // 
+            this.averageLabel.AutoSize = true;
+            this.averageLabel.Location = new System.Drawing.Point(12, 97);
+            this.averageLabel.Name = "averageLabel";
+            this.averageLabel.Size = new System.Drawing.Size(153, 39);
+            this.averageLabel.TabIndex = 3;
+            this.averageLabel.Text = "Average:";
+            // 
+            // modeLabel
+            // 
+            this.modeLabel.AutoSize = true;
+            this.modeLabel.Location = new System.Drawing.Point(12, 212);
+            this.modeLabel.Name = "modeLabel";
+            this.modeLabel.Size = new System.Drawing.Size(120, 39);
+            this.modeLabel.TabIndex = 4;
+            this.modeLabel.Text = "Mode: ";
+            // 
+            // histogram
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.histogram.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.histogram.Legends.Add(legend1);
+            this.histogram.Location = new System.Drawing.Point(535, 97);
+            this.histogram.Name = "histogram";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.histogram.Series.Add(series1);
+            this.histogram.Size = new System.Drawing.Size(505, 300);
+            this.histogram.TabIndex = 5;
+            this.histogram.Text = "Chart";
+            this.histogram.Click += new System.EventHandler(this.histogram_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(19F, 38F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1052, 453);
+            this.Controls.Add(this.histogram);
+            this.Controls.Add(this.modeLabel);
+            this.Controls.Add(this.averageLabel);
+            this.Controls.Add(this.medianLabel);
+            this.Controls.Add(this.calcBtn);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 7, 7, 7);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.histogram)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.HelpProvider helpProvider1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button calcBtn;
+        private System.Windows.Forms.Label medianLabel;
+        private System.Windows.Forms.Label averageLabel;
+        private System.Windows.Forms.Label modeLabel;
+        private System.Windows.Forms.DataVisualization.Charting.Chart histogram;
+    }
+}
+
---
+++ Project231/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project231
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void calcBtn_Click(object sender, EventArgs e)
+        {
+            string text = textBox1.Text;
+            List<double> nums = new List<double>();
+            string[] str = text.Split(' ');
+            foreach (var item in str)
+            {
+                nums.Add(double.Parse(item));
+            }
+
+            averageLabel.Text.Remove(0);
+            averageLabel.Text = "Average: " + Average(nums);
+
+            medianLabel.Text.Remove(0);
+            medianLabel.Text = "Median: " + Median(nums);
+
+            modeLabel.Text.Remove(0);
+            modeLabel.Text = "Mode: " + Mode(nums);
+
+            histogram.Series[0].Points.Clear();
+            foreach (double item in nums)
+            {
+                histogram.Series[0].Points.Add(item);
+            }
+        }
+
+        private double Median(List<double> list)
+        {
+            list.Sort();
+            if (list.Count % 2 != 0)
+            {
+                return list[list.Count / 2];
+            }
+            return (list[list.Count / 2] + list[(list.Count-1) / 2]) / 2;
+        }
+
+        private double Average(List<double> list)
+        {
+            double sum = 0;
+            foreach (double item in list)
+            {
+                sum += item;
+            }
+            return sum/ list.Count;
+        }
+
+        private double Mode(List<double> list)
+        {
+            double mode = 0;
+            int maxcount = 0;
+            for (int i = 0; i < list.Count; i++)
+            {
+                int count = 0;
+                for (int j = 0; j < list.Count; j++)
+                {
+                    if (list[i] == list[j])
+                    {
+                        count++;
+                    }
+                }
+                if (count > maxcount)
+                {
+                    mode = list[i];
+                    maxcount = count;
+                }
+            }
+            return mode;
+        }
+
+        private void histogram_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
